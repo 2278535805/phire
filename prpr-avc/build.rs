3a1a747c13c6c06782124ea5a4f23718620f129d
HLMC <hl2278535805@outlook.com>
2026-08-22 01:20:28 +0800

    update ffmpeg

--- prpr-avc/build.rs
+++ prpr-avc/build.rs
@@ -4,7 +4,7 @@ use std::{
     time::Duration,
 };
 
-const BASE_URL: &str = "https://github.com/TeamFlos/prpr-avc-ffmpeg/releases/download";
+const BASE_URL: &str = "https://github.com/2278535805/prpr-avc-ffmpeg/releases/download";
 const EXPECTED_LIBS: &[&str] = &["libavcodec.a", "libavformat.a", "libavutil.a", "libswresample.a", "libswscale.a"];
 
 fn main() {
