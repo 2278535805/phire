e4732c9d94d4486ba2c291a552bee95192183783
HLMC <hl2278535805@outlook.com>
2026-09-21 21:51:03 +0800

    fix: tag edit

--- phire-ui/src/tags.rs
+++ phire-ui/src/tags.rs
@@ -111,11 +111,13 @@ impl Tags {
             draw(btn, tag);
         }
         if self.add_input.is_active() {
-            let w = mw - x;
-            if w > 0.08 {
-                let r = Rect::new(x, h, w, row_height).feather(-pad);
-                self.add_input.render(ui, r, alpha, &tl!("edit"));
+            let bw = ui.text("+").size(sz).measure().w.clamp(0.08, tmw) + (margin + pad) * 2.;
+            if x + bw > mw {
+                x = 0.;
+                h += row_height;
             }
+            let r = Rect::new(x, h, mw - x, row_height).feather(-pad);
+            self.add_input.render(ui, r, alpha, &tl!("edit"));
         } else {
             draw(&mut self.add, "+");
         }
